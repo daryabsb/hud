7da7ac268188d8e8e282a793108040f8d0860d53
Darya Ibrahim <daryabsb@gmail.com>
2024-05-28 22:53:30 +0300

    Working on creating the settings page

--- src/configurations/admin/admin_application_property.py
+++ src/configurations/admin/admin_application_property.py
@@ -1,17 +1,7 @@
 from django.contrib import admin
 from src.accounts.models import User
 from src.configurations.models import ApplicationProperty
-
-
-INITIAL_DATA = [
-    {"id": 1, "name": "Theme.mode", "value": 'dark'},
-    {"id": 2, "name": "Theme.color", "value": 'pink'},
-    {"id": 3, "name": "Theme.cover", "value": '1'},
-    {"id": 3, "name": "Site.title", "value": 'Zeneon LLC.'},
-    {"id": 3, "name": "Site.language", "value": 'en-us'},
-    {"id": 3, "name": "Site.writing_direction", "value": 'left_to_right'},
-    {"id": 3, "name": "Site.cover", "value": '1'},
-]
+from src.configurations.const import INITIAL_DATA
 
 
 @admin.register(ApplicationProperty)
@@ -20,15 +10,13 @@ class ApplicationPropertyAdmin(admin.ModelAdmin):
     ordering = ('created', )
     list_filter = ('name', )
 
-    # @staticmethod
-    # def initial_data():
-    #     for index, cur in enumerate(INITIAL_DATA):
-    #         currency = ApplicationProperty.objects.filter(name=cur['name']).first()
-    #         if not currency:
-    #             user = User.objects.get(email='root@root.com')
-    #             currency = ApplicationProperty(**cur)
-    #             currency.user = user
-    #             currency.save(force_insert=True)
-    #         else:
-    #             currency.code = cur['code']
-    #             currency.save(force_update=True)
+    @staticmethod
+    def initial_data():
+        for index, cur in enumerate(INITIAL_DATA):
+            setting = ApplicationProperty.objects.get(name=cur['name'])
+            if not setting:
+                setting = ApplicationProperty(**cur)
+                setting.save(force_insert=True)
+            else:
+                setting.name = cur['name']
+                setting.save(force_update=True)
