9607179883d54a40f6419788f762c6c222b95338
Darya Ibrahim <daryabsb@gmail.com>
2024-11-04 07:47:14 +0300

    Working through order form

--- src/orders/forms.py
+++ src/orders/forms.py
@@ -21,8 +21,9 @@ class DocumentForm(forms.ModelForm):
         )
     )
     customer = forms.ModelChoiceField(
-        queryset=Customer.objects.filter(is_customer=True),
-        required=False, label='Customer',
+        # queryset=Customer.objects.filter(is_customer=True),
+        queryset=Customer.objects.none(),
+        required=False, label=None,
         to_field_name='id',
         widget=forms.Select(
             attrs={'class': 'form-select form-select-sm'}
@@ -63,7 +64,7 @@ class DocumentForm(forms.ModelForm):
             'customer',
             'discount_type',
             'discount',
-            )
+        )
         # fields = '__all__'
 
     def __init__(self, *args, **kwargs):
@@ -74,9 +75,18 @@ class DocumentForm(forms.ModelForm):
         if initial:
             document_type = initial.get('document_type')
         if document_type:
-            print(document_type.name)
-        
-        print(self._meta.fields[2])
+            # print(document_type.name)
+
+            if document_type.category.id == 1:
+                print('Purchase')
+            elif document_type.category.id == 2:
+                print('Sale')
+            elif document_type.category.id == 3:
+                print('Inventory')
+            elif document_type.category.id == 4:
+                print('Loss and Damage')
+
+        # print(self._meta.fields[2])
 
 
 class CreateSaleForm(forms.Form):
@@ -123,3 +133,42 @@ class CreateSaleForm(forms.Form):
             }
         )
     )
+
+
+'''
+from src.orders.forms import DocumentForm
+from src.accounts.models import User
+from src.documents.models import DocumentType
+
+
+dt = DocumentType.objects.get(id=3)
+user = User.objects.first()
+
+form = DocumentForm(initial={'document_type':dt,'user':user})
+
+print(form['number'].value())
+
+print(form['customer'].value()) 
+None
+form.save()
+
+document = form.save(commit=False)
+document.user = user
+document.save()
+
+
+
+document.document_type = dt
+document.save()
+
+[
+    'Meta', 'add_error', 'add_initial_prefix', 'add_prefix', 'as_div', 'as_p', 
+    'as_table', 'as_ul', 'base_fields', 'changed_data', 'clean', 'declared_fields', 
+    'default_renderer', 'errors', 'field_order', 'full_clean', 'get_context', 
+    'get_initial_for_field', 'has_changed', 'has_error', 'hidden_fields', 'is_multipart', 
+    'is_valid', 'media', 'non_field_errors', 'order_fields', 'prefix', 'render', 'save', 
+    'template_name', 'template_name_div', 'template_name_label', 'template_name_p', 
+    'template_name_table', 'template_name_ul', 'use_required_attribute', 'validate_unique', 
+    'visible_fields'
+    ]
+'''
