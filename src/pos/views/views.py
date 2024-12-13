ed136a2f56ef77396d8f7494e4cd479497bf2793
Darya Ibrahim <daryabsb@gmail.com>
2024-12-13 21:19:08 +0300

    The new face of pos standard created

--- src/pos/views/views.py
+++ src/pos/views/views.py
@@ -3,6 +3,7 @@ from src.products.models import Product, ProductGroup
 from src.orders.models import PosOrder
 from src.pos.utils import activate_order_and_deactivate_others as aod
 from django.contrib.auth.decorators import login_required
+from src.configurations.models import ApplicationProperty
 # Create your views here.
 
 
@@ -10,6 +11,9 @@ from django.contrib.auth.decorators import login_required
 def pos_home(request, id=None):
     user = request.user
     active_order = aod(user, id)
+    layout_object = ApplicationProperty.objects.get(name='layout')
+
+    print(layout_object.value)
 
     product_groups = ProductGroup.objects.filter(parent__isnull=True)
     pos_orders = PosOrder.objects.all()
@@ -21,4 +25,6 @@ def pos_home(request, id=None):
         "products": products,
         "orders": pos_orders,
     }
-    return render(request, 'pos/pos-home.html', context)
+    if layout_object.value == 'standard':
+        return render(request, 'pos/standard/pos-home.html', context)
+    return render(request, 'pos/visual/pos-home.html', context)
