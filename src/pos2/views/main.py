660773a1ae0aec2bb69d7aa4108f8ff138208283
Darya Ibrahim <daryabsb@gmail.com>
2025-07-20 16:03:21 +0300

    feat(pos): add menu functionality and restructure order view
    - Add menu template and integrate with main view
    - Include menus in order context from configurations
    - Restructure order view layout with menu navigation
    - Update VSCode settings for HTML formatting

--- src/pos2/views/main.py
+++ src/pos2/views/main.py
@@ -1,22 +1,25 @@
 from django.shortcuts import render
-from src.orders.models import get_orders #, PosOrder
+from src.orders.models import get_orders  # , PosOrder
 from src.pos.utils import get_active_order, activate_order_and_deactivate_others as aod
 from django.contrib.auth.decorators import login_required
+from src.configurations.models import get_menus
 
 
 @login_required
 def pos_home(request, nunmber=None):
     orders = get_orders(user=request.user)
-    context = {'orders': orders,}
+    context = {'orders': orders, }
     return render(request, 'cotton/pos/home/index.html', context)
+
 
 @login_required
 def pos_order(request, number):
     orders = get_orders(user=request.user)
     active_order = aod(request.user, order_number=number)
-    
+
     context = {
         'orders': orders,
+        'menus': get_menus(),
         'active_order': active_order,
-        }
+    }
     return render(request, 'cotton/pos/order/index.html', context)
